262574524fd9235c07f91015b1efc84bbcd2236f
Matheusfelipea <matheusfelipe_801@hotmail.com>
2022-03-21 21:12:07 -0300

    Index get

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Oi gente");
+
+console.log("Muita emoção....meu servidor....");
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/qualSeuNome', (req, res) => {
+  res.send('Me chamo um servidor teste!')
+})
+
+app.get('/usuarios/:id', (req, res) => {
+  let id = req.params.id;
+  res.send('lista de usuários...'+ id)
+})
+
+app.listen(port, () => {
+  console.log(`Sou um servidor teste ${port}`)
+})
+
